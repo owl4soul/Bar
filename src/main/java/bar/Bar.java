d444250e70392e0b31192d54ba083a9a688f1f79
owl4soul <resuuser989@gmail.com>
2019-03-20 22:22:59 +0300

    0.2.0 Prices. Try-catches...Tests...

--- src/main/java/bar/Bar.java
+++ src/main/java/bar/Bar.java
@@ -1,13 +1,21 @@
 package bar;
-//todo убрать класс
+//Цены считаем здесь
+
+import bar.drinks.Drink;
+
 public class Bar {
+    private static int shotPrice = 20;
+    private static int milkPrice = 30;
+    private static int waterPrice = 3;
+    private static int sugarPrice = 1;
+    private static int cinnamonPrice = 2;
+    private static int icePrice = 4;
 
-    public void brewDrink() {
-
-    }
-
-    public void countTotalPrice() {
-
+    public static int countTotalPrice(Drink drink) {
+        int totalShot = drink.getShot() * shotPrice;
+        int totalMilk = drink.getMilk() * milkPrice;
+        System.out.println(totalMilk+totalShot);
+        return totalShot + totalMilk;
     }
 
 
--- src/main/java/bar/Program.java
+++ src/main/java/bar/Program.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 
 public class Program {
     static private boolean endConversation;
-    static Scanner scan = new Scanner(System.in);
+    static Scanner scan;
     static Drink drink = new Drink();
     static Stock stock = new Stock();
 
     public static void main(String[] args) {
-
+        scan = new Scanner(System.in);
         stock.showStock();
 
         conversation();
@@ -20,84 +20,94 @@ public class Program {
 
     static void conversation() {
         while (!endConversation) {
+
             askDrink();
             stock.showStock();
             askSupplements(drink);
             stock.showStock();
             askAgain(drink);
             stock.showStock();
+            Bar.countTotalPrice(drink);
 
 
         }
     }
 
     static void askDrink() {
-        try {
-            System.out.println("Что Вам приготовить? Введите номер: ");
-            int input = scan.nextInt();
-            if (input < 1 || input > 3) {
-                throw new IllegalArgumentException();
-            } else {
-                drink.classicDrink(input);
-                System.out.println("шотов " + drink.getShot());
-                System.out.println("молока " + drink.getMilk());
-                drink.showDrink();
-                return;
-            }
 
-        } catch (InputMismatchException e) {
-            System.out.println(e.toString());
-            System.out.println("ОШИБКА! Возможно, были введены буквы или другие символы, кроме цифр, попробуйте снова.");
-            askDrink();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.toString());
-            System.out.println("ОШИБКА! В меню нет такого напитка, повторите ввод.");
-            askDrink();
+        System.out.println("Что Вам приготовить? Введите номер: ");
+        String input = scan.next();
+        if (!stopAsking(input)) {
+            try {
+                int num = Integer.parseInt(input);
+                if (num < 1 || num > 3) {
+                    throw new IllegalArgumentException();
+                } else {
+                    drink.classicDrink(num);
+                    System.out.println("шотов " + drink.getShot());
+                    System.out.println("молока " + drink.getMilk());
+                    drink.showDrink();
+                    return;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println(e.toString());
+                System.out.println("ОШИБКА! Возможно, были введены буквы или другие символы, кроме цифр, попробуйте снова.");
+                askDrink();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.toString());
+                System.out.println("ОШИБКА! В меню нет такого напитка, повторите ввод.");
+                askDrink();
+            }
+        } else {
+            System.out.println("Вы отказались делать заказ. Всего доброго");
+            System.exit(0);
         }
     }
 
     static void askSupplements(Drink drink) {
         Drink tmpDrink = drink;
-        boolean stopAskSupplements = false;
-        do {
-            try {
-                System.out.println("Что добавить в Ваш напиток? Введите номер: ");
-                String input = scan.next();
-
-                while (!stopAsking(input)) {
-                    int num = Integer.parseInt(input);
-                    drink.totalDrink(num);
-                    System.out.println("в напитке шотов: " + drink.getShot());
-                    System.out.println("в напитке молока: " + drink.getMilk());
-                    return;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println(e.toString());
-                System.out.println("ОШИБКА! Возможно, были введены буквы или другие символы, кроме цифр, перечислите добавки заново.");
-                drink = tmpDrink;
-                askSupplements(drink);
+        System.out.println("Что добавить в Ваш напиток? Введите номер: ");
+        String input = scan.next();
+        try {
+            if (stopAsking(input)) {
+                return;
+            } else {
+                int num = Integer.parseInt(input);
+                drink.totalDrink(num);
+                System.out.println("в напитке шотов: " + drink.getShot());
+                System.out.println("в напитке молока: " + drink.getMilk());
+                return;
             }
-        } while (!stopAskSupplements);
+        } catch (InputMismatchException e) {
+            System.out.println(e.toString());
+            System.out.println("ОШИБКА! Возможно, были введены буквы или другие символы, кроме цифр, перечислите добавки заново.");
+            drink = tmpDrink;
+            askSupplements(drink);
+        } catch (NumberFormatException e) {
+            System.out.println(e.toString());
+            System.out.println("ОШИБКА! Невозможно прочитать цифры.");
+            askSupplements(drink);
+        }
+
     }
 
     static void askAgain(Drink drink) {
         System.out.println("Может, еще что-нибудь?");
         String input = scan.next();
-            if (stopAsking(input)) {
-                return;
-            } else {
-                try {
-                    int num = Integer.parseInt(input);
-                    drink.totalDrink(num);
-                    askAgain(drink);
-                } catch (NumberFormatException e) {
-                    System.out.println(e.toString());
-                }
+        if (stopAsking(input)) {
+            return;
+        } else {
+            try {
+                int num = Integer.parseInt(input);
+                drink.totalDrink(num);
+                askAgain(drink);
+            } catch (NumberFormatException e) {
+                System.out.println(e.toString());
             }
+        }
 
     }
-
-
 
 
     static boolean stopAsking(String input) {
@@ -105,13 +115,27 @@ public class Program {
             System.out.println("Раз Вы молчите, значит ничего не нужно.");
             return true;
         } else if (input.equalsIgnoreCase("stop")
-                || input.equalsIgnoreCase("стоп")
-                || input.equalsIgnoreCase("отстань")) {
+                || input.equalsIgnoreCase("стоп")) {
             System.out.println("Как скажете.");
-            return true;
+            System.out.println("Общая стоимость напитка: ");
+            Bar.countTotalPrice(drink);
+
+            System.out.println("Завершить программу? Нажмите 0 для выхода из программы.");
+            try {
+                int num = scan.nextInt();
+                if (num == 0) {
+                    System.exit(0);
+                } else {
+                    return true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Хм...тогда продолжим");
+            }
+
         } else {
             return false;
         }
+        return false;
     }
 
 }
